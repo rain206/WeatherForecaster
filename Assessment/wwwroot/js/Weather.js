40232d94e16ec5545606fdaee0bb476797b96d03
Craig Rainey <craig.rainey@allyis.com>
2017-09-06 12:31:33 -0700

    Update 3 -- Color schemes, icons, and using session location.

--- Assessment/wwwroot/js/Weather.js
+++ Assessment/wwwroot/js/Weather.js
@@ -1,6 +1,27 @@
 ﻿var autocomplete;
 var place;
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var city;
+var changed = 0;
+
+$(document.ready).ready(function () {
+	getLocation();
+})
+
+function getLocation() {
+	var location;
+	$.ajax({
+		format: "jsonp",
+		dataType: "jsonp",
+		url: "http://ip-api.com/json",
+		success: function (data) {
+			location = (data.lat + "," + data.lon);
+			city = data.city;
+			getForecast(location);
+		},
+		method: "GET"
+	});
+}
 
 function initMap() {
 	var acOptions = {
@@ -16,7 +37,7 @@ function initMap() {
 
 function getWeather() {
 	var loc = autocomplete.getPlace();
-	var latLon = loc.geometry.location.lat() + ", " + loc.geometry.location.lng();
+	var latLon = loc.geometry.location.lat() + "," + loc.geometry.location.lng();
 	getCity(latLon);
 	getForecast(latLon);
 }
@@ -29,7 +50,7 @@ function getCity(latLon) {
 		dataType: "json",
 		url: url,
 		success: function (data) {
-			$('#weather-location').html(data.results[0].address_components[2].long_name);
+			city = data.results[0].address_components[3].long_name
 		},
 		method: "GET"
 	});
@@ -42,19 +63,31 @@ function getForecast(latLon) {
 		dataType: "jsonp",
 		url: url,
 		success: function (json) {
+			var day = new Date();
+			$("#weather-location").html(days[day.getDay()] + " in " + city);
 			$("#weather-current").html(Math.round(json.currently.temperature) + "°");
-			$("#weather-high").html("High: " + Math.round(json.daily.data[2].temperatureMax) + "°");
-			$("#weather-low").html("Low: " + Math.round(json.daily.data[1].temperatureMin) + "°");
-			$("#summary").html(json.daily.data[1].summary);
+			$("#weather-high").html("High: " + Math.round(json.daily.data[0].temperatureMax) + "°");
+			$("#weather-low").html("Low: " + Math.round(json.daily.data[0].temperatureMin) + "°");
+			$("#summary").html(json.daily.data[0].summary);
 
 			for (var i = 1; i <= 6; i++)
 			{
 				var date = new Date();
 				var newDate = new Date(date.setTime(date.getTime() + (i * 86400000)));
-				$("#day" + i).html(days[newDate.getDay()] + ":")
-				$("#day" + i + "-high").html("High: " + Math.round(json.daily.data[i].temperatureMax))
+				$("#day" + i).html(days[newDate.getDay()]);
+
+				if (changed == 1)
+				{
+					$("#day" + i).next().replaceWith(getWeatherIcon(json.daily.data[i].icon))
+				}
+				else {
+					document.getElementById("day" + i).insertAdjacentHTML('afterend', getWeatherIcon(json.daily.data[i].icon));
+				}
+
+				$("#day" + i + "-high").html("High: " + Math.round(json.daily.data[i].temperatureMax));
 				$("#day" + i + "-low").html("Low: " + Math.round(json.daily.data[i].temperatureMin));
 			}
+			changed = 1;
             historicalGraph(url);
             $("#info").show(1000);
 			$("#forecast").show(1000);
@@ -63,6 +96,27 @@ function getForecast(latLon) {
 	.error(function (jqXHR, textStatus, errorThrown) {
 		alert("Forecast error. Please reenter your location");
 	})
+}
+
+function getWeatherIcon(icon)
+{
+	if (icon.localeCompare('clear-night') == 0)
+	{
+		return '<i class=fa fa-star fa-2x aria-hidden="true"></i>';
+	}
+	else if (icon.localeCompare('rain') == 0)
+	{
+		return '<i class="fa fa-shower fa-2x" aria-hidden="true"></i>'
+	}
+	else if (icon.localeCompare('snow') == 0 || icon.localeCompare('sleet') == 0)
+	{
+		return '<i class="fa fa-snowflake-o fa-2x" aria-hidden="true"></i>'
+	}
+	else if (icon.localeCompare('fog') == 0 || icon.localeCompare('cloudy') == 0 || icon.localeCompare('partly-cloudy-day') == 0)
+	{
+		return '<i class="fa fa-cloud fa-2x" aria-hidden="true"></i>'
+	}
+	return '<i class="fa fa-sun-o fa-2x" aria-hidden="true"></i>'
 }
 
 function historicalGraph(url) {
@@ -105,7 +159,8 @@ function renderGraph(xAxis, yAxis)
     var data = [
         {
             x: xAxis,
-            y: yAxis,
+			y: yAxis,
+			//marker: {color: 'rgb(0, 0, 0)'},
             type: 'bar'
         }
     ]
@@ -117,7 +172,9 @@ function renderGraph(xAxis, yAxis)
         },
         yaxis: {
             title: 'Degrees (F)'
-        }
+		},
+		paper_bgcolor: 'rgba(0, 0, 0, 0)',
+		plot_bgcolor: 'rgba(0, 0, 0, 0)'
     };
     Plotly.newPlot('graph', data, layout);
     return;
